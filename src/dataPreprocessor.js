2903591edd1670baf5712d26642d798ece38a408
majsy <madeleineauvinen@gmail.com>
2019-03-05 14:23:43 -0500

    add func to get current coordinates add func to get nearest fountain based on coordinates install haversine-distance

--- src/dataPreprocessor.js
+++ src/dataPreprocessor.js
@@ -31,10 +31,10 @@ const codeAddress = (address) => {
   return new Promise((resolve, reject) => {
     geocoder.geocode({'address': address}, async (results, status) => {
       if (status === 'OK') {
-        resolve([
-          results[0].geometry.location.lat(),
-          results[0].geometry.location.lng()
-        ])    
+        resolve({
+          lat: results[0].geometry.location.lat(),
+          lon: results[0].geometry.location.lng()
+        })    
       } else {
         reject(status);
       }
--- src/index.js
+++ src/index.js
@@ -2,69 +2,91 @@
 
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import data from './data/fountainsSubset.json'
-
+import data from './data/fountainsSubset.json';
 import { run } from './dataPreprocessor'
 
+const haversine = require('haversine-distance');
 // run()
 
-const addressForm = document.querySelector('form.submit-address-form')
-const submitAddressButton = addressForm.querySelector('button');
-const addressInput = addressForm.querySelector('input');
-const address = addressInput.value;
-
+const form = document.querySelector('form.submit-address-form')
+const submitButton = form.querySelector('button');
+const input = form.querySelector('input');
 const getlocationButton = document.querySelector('button.get-location');
+
 const geocoder = new google.maps.Geocoder();
 
-// get users geolocation
+// get users current coordinates
 const getLocation = () => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(logPosition);
-  } else {
-    console.log('Geolocation is not supported by this browser.')
+  return new Promise((resolve, reject) => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position, error) => {
+        if (error) {
+          reject(error)
+        } else {
+          resolve({
+            lat: position.coords.latitude,
+            lon: position.coords.longitude
+          })
+        }
+      });
+    } else {
+      reject('Geolocation is not supported by this browser.')
+    }
+  })
+}
+
+// get input address coordinates
+const codeAddress = (address) => {
+  return new Promise((resolve, reject) => {
+    geocoder.geocode({'address': address}, (results, status) => {
+      if (status === 'OK') {
+        resolve({
+          lat: results[0].geometry.location.lat(),
+          lon: results[0].geometry.location.lng()
+        })  
+      } else {
+        reject(status);
+      }
+    });
+  })
+}
+
+// get nearest fountain based on coordinates
+const getNearestFountain = (userCoordinates) => {
+  const distanceList = data.map(fountain =>
+    haversine(userCoordinates, fountain.coordinates))
+
+  const sorted = [...distanceList].sort((a, b) => a - b);
+  const nearest = sorted[0];
+
+  const index = distanceList.indexOf(nearest);
+
+  return data[index]
+}
+
+
+// Event Listeners
+getlocationButton.addEventListener('click', async () => {
+  try {
+    const coordinates = await getLocation()
+    const nearest = getNearestFountain(coordinates)
+    console.log(nearest)
+  } catch(e) {
+    console.log('address is not valid')
   }
-}
-
-// log users location coordinates
-const logPosition = (position) => {
-  console.log('latitude:', position.coords.latitude, 'longitude:', position.coords.longitude )
-}
-
-
-// get address coordinates
-// const codeAddress = (address) => {
-//   return new Promise((resolve, reject) => {
-//     geocoder.geocode({'address': address}, async (results, status) => {
-//       if (status === 'OK') {
-//         resolve([
-//           results[0].geometry.location.lat(),
-//           results[0].geometry.location.lng()
-//         ])  
-//       } else {
-//         reject(status);
-//       }
-//     });
-//   })
-// }
-
-
-// export const getAddressCoordinates = async (address) => {
-//   // const address = addressInput.value;
-//   // const borough = getBorough(data[index]['Borough']);
-
-//   const coordinates = await codeAddress(`${address}, New York, NY`);
-//   console.log(coordinates)
-// }
-
-// event listeners
-getlocationButton.addEventListener('click', () => {
-  getLocation()
 })
 
-// submitAddressButton.addEventListener('click', () => {
-//   getAddressCoordinates(address)
-// })
+submitButton.addEventListener('click', async () => {
+  const address = input.value;
 
+  try {
+    const coordinates = await codeAddress(`${address}, New York`)
+    const nearest = getNearestFountain(coordinates)
+    console.log(nearest)
+  } catch(e) {
+    console.log('address is not valid')
+  }
+})
 
 
 
